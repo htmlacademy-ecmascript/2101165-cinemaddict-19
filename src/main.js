7ae543a619cbd9a60bdeee75fe00cb4c6c4fd930
Dmitry Bochenkov <pishisudapridurok@yandex.ru>
2023-01-30 20:27:17 +0300

    3.10. Революция или эволюция? (часть 1) исправил конфликт

--- src/main.js
+++ src/main.js
@@ -5,11 +5,6 @@ import SortView from './view/sort-view.js';
 import FilmModel from './model/film-model.js';
 import FilmPresenter from './presenter/film-presenter.js';
 import FooterStatisticView from './view/footer-statistic-view.js';
-import PopupPresenter from './presenter/popup-presenter.js';
-
-
-// import PopupWholeView from './view/popup-whole-view.js';
-import CommentModel from './model/comment-model.js';
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
@@ -22,11 +17,6 @@ const filmPresenter = new FilmPresenter({
   filmModel,
 });
 
-// const popupPresenter = new PopupPresenter({
-//   container: siteMainElement,
-//   filmModel
-// });
-
 render (new ProfileView(), siteHeaderElement);
 render(new FilterView(), siteMainElement);
 render(new SortView(), siteMainElement);
@@ -34,7 +24,3 @@ render(new SortView(), siteMainElement);
 filmPresenter.init();
 
 render(new FooterStatisticView(), siteFooterElement);
-
-// popupPresenter.init();
-
-// render(new PopupWholeView(), siteMainElement);
