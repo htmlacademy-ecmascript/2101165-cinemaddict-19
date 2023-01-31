2a1d9d45b20c54a58f2b6fbe2adc78707a5ec0f4
Dmitry Bochenkov <pishisudapridurok@yandex.ru>
2023-01-31 23:10:30 +0300

    ДЗ 3.2

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 import {render} from './render.js';
 import ProfileView from './view/profile-view.js';
-import FilterView from './view/filter-view.js';
-import SortView from './view/sort-view.js';
 import FilmModel from './model/film-model.js';
 import FilmPresenter from './presenter/film-presenter.js';
-import FooterStatisticView from './view/footer-statistic-view.js';
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
-const siteFooterElement = document.querySelector('.footer__statistics');
 
 const filmModel = new FilmModel();
 
@@ -18,9 +14,6 @@ const filmPresenter = new FilmPresenter({
 });
 
 render (new ProfileView(), siteHeaderElement);
-render(new FilterView(), siteMainElement);
-render(new SortView(), siteMainElement);
 
 filmPresenter.init();
 
-render(new FooterStatisticView(), siteFooterElement);
